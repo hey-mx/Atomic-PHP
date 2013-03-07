3bcadd9b3178d6c44bbc940acb65f3f1f58994f0
Juan Cabrera <jcabrera@jobomas.com>
2013-03-07 17:19:06 -0600

    Pequeña documentación

--- lib/PhpRoute.php
+++ lib/PhpRoute.php
@@ -4,7 +4,9 @@ class PhpRoute extends Route
 {
 
     protected $staticElements=array();
-
+	/**
+	* Funcion para agregar variables estáticas en los argumentos
+	*/
     public function addStaticElement($key,$value){
         $this->staticElements[$key] = $value;
         return $this;
@@ -16,7 +18,7 @@ class PhpRoute extends Route
     
     public function getMapArguments()
     {        
-        
+        // combina lo que regresa el router +  las variables estáticas
         return array_merge(parent::getMapArguments(),  $this->staticElements);
     }
 }
