13d6dd40bc43ada533be3b878cd015a5e7454cbd
maikel <rmayo@redgps.com>
2014-11-28 15:59:54 -0600

    Arreglando bug

--- Atomic.php
+++ Atomic.php
@@ -215,7 +215,7 @@ class Atomic {
                 }
             } else {
                 $action = (!isset(self::$system['action_prefix']) ? '' : 
-                self::$system['action_prefix']) . Index;
+                self::$system['action_prefix']) . 'Index';
                 $content->$action();
             }
         } catch(ActiveRecord\DatabaseException $e) {
